e9a077d52701645db0c6d946ed1fd223f311f86b
mvp18 <soumava2016@gmail.com>
2022-08-07 17:10:51 +0200

    sample runs for barlow ttt

--- src/algos/SnMpNet/run_script.sh
+++ src/algos/SnMpNet/run_script.sh
@@ -4,7 +4,11 @@
 #SBATCH -o /BS/UCDR/work/slurm/slurm-%A_%a.out
 #SBATCH -t 2-0
 
-cmd="python3 test.py -sd quickdraw -hd sketch -gd real"
+# cmd="python3 ttt_barlow.py -sd quickdraw -hd sketch -gd real -e 3 -bs 128 -lrc 1e-5"
+# cmd="python3 ttt_barlow.py -sd sketch -hd quickdraw -gd real -e 3 -bs 128 -lrc 1e-5"
+# cmd="python3 ttt_barlow.py -sd painting -hd clipart -gd real -e 3 -bs 128 -lrc 1e-5"
+# cmd="python3 ttt_barlow.py -sd painting -hd infograph -gd real -e 3 -bs 128 -lrc 1e-5"
+cmd="python3 ttt_barlow.py -sd infograph -hd painting -gd real -e 3 -bs 128 -lrc 1e-5"
 
 # print start time and command to log
 echo $(date)
